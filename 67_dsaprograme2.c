3daa43543fc29c0579c4a18834cc17249ef10085
chrisrodricks11405 <140340917+chrisrodricks11405@users.noreply.github.com>
2024-08-07 10:05:56 +0530

    Create 67_dsaprograme2.c

---
+++ 67_dsaprograme2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int x;
+int a,b,c,d;
+a=0;
+b=1;
+printf("Enter the number");
+scanf("%d",&x);
+if(x==a || x==b)
+{
+printf("%d is a fibbonaci",x);
+}
+
+if(b<=x);
+{
+a=b;
+c=b;
+b=a+b;
+if(b==d)
+{
+d=1;
+}
+}
+if(d)
+{
+printf("\n %d is a fibbonaci",x);
+}
+ 
+else
+{
+printf("\n %d is not a fibbonaci",x);
+}
+return 0;
+}
+
+ 
